c317572f0228a3acc5a7cc7baf0b5df20c63074c
Joey <3288432965@qq.com>
2022-09-22 18:40:05 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import APP from './login';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
     <APP />,
--- src/login/index.js
+++ src/login/index.js
@@ -1,32 +1,61 @@
 import React, { useState } from 'react';
-import { Form, Input, Radio, CheckBox, Button } from 'antd';
+import { Form, Input, Tabs, Checkbox, Button } from 'antd';
 import './index.css';
 
 const Login = () => {
   const [form] = Form.useForm();
-  const [loginType, setLoginType] = useState(1);
+  const [loginDisabled, setLoginDisabled] = useState(true);
 
   function onFinish() {
-
+    form.validateFields().then(v => {
+      console.log(v);
+    })
   }
 
-  function changeLoginType(e) {
-    setLoginType(e.target.value)
+  async function onChange() {
+    const values = await form.validateFields();
+    console.log(values);
+    const {mobile = '', password = '' } = values;
+    if (mobile && password) {
+      setLoginDisabled(false);
+      return;
+    }
+    setLoginDisabled(true);
   }
 
   return (
-    <Form form={form} onFinish={onFinish}>
+    <Form form={form} onFinish={onFinish} onValuesChange={onChange}>
       <div className="login">
-        <Form.Item><Radio.Group onChange={changeLoginType} value={loginType}>
-          <Radio.Button value={1}>短信登录</Radio.Button>
-          <Radio.Button value={2}>密码登录</Radio.Button>
-        </Radio.Group></Form.Item>
-        {loginType === 1 && (
-          <div>111</div>  
-        )}
-        {loginType === 2 && (
-          <div>222</div>
-        )}
+        <Form.Item><Tabs>
+          <Tabs.TabPane tab="短信登录" key={1}><div>111</div>  </Tabs.TabPane>
+          <Tabs.TabPane tab="密码登录" key={2}>
+            <div className="login_password">
+              <Form.Item name="mobile">
+                <Input className="login_password-input" placeholder="手机号/邮箱"/>
+              </Form.Item>
+              <Form.Item name="password">
+                <Input className="login_password-input" placeholder="密码"/>
+              </Form.Item>
+              <Form.Item>
+                <Button className="login_password-btn" 
+                  htmlType='submit' 
+                  disabled={loginDisabled}
+                >登录</Button>
+              </Form.Item>
+              <Form.Item className="login_password-form-footer">
+                <Form.Item name="check" valuePropName="checked">
+                  <Checkbox value={1}>下次自动登录</Checkbox>
+                </Form.Item>
+                <div className="login_password-form-footer-right">
+                  <a href='' target="_blank">忘记密码</a>
+                  &nbsp;｜&nbsp;
+                  <a href='' target="_blank">注册</a>
+                </div>
+              </Form.Item>
+              
+            </div>
+          </Tabs.TabPane>
+        </Tabs></Form.Item>
       </div>
     </Form>
   )
